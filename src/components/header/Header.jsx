9152bd9821480efb7109ec394dcdba45bfa47017
Yubraj <imyubraz@gmail.com>
2023-05-14 13:30:10 +0545

    Navbar setup done...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <>
-            <div className="wrapper min-h-[10vh] bg-purple-900 text-white">
-                <div className="container max-w-[1280px] mx-auto">
-                    Header
+            <div className="wrapper min-h-[10vh] bg-purple-900 text-white flex items-center">
+                <div className="container max-w-[1280px] mx-auto p-2">
+                    <div className="row flex gap-8">
+                        <div className="col">
+                            <div className="logo">
+                                <p className='font-bold text-2xl'>Contact App 📞</p>
+                            </div>
+                        </div>
+                        <div className="col grow">
+                            <div className="menu-wrapper text-xl">
+                                <ul className='menus flex gap-8'>
+                                    <li className='menu'><NavLink to={`/contacts`}>All Contacts</NavLink></li>
+                                    <li className='menu'><NavLink to={`/add-contact`}>Add Contact</NavLink></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Layout from '../../layouts/Layout'
 
 const NotFound = () => {
     return (
-        <div className='h-screen flex justify-center items-center'>
-            <h2 className='font-semibold text-3xl text-red-600'>Error 404 : Page Not Found</h2>
-        </div>
+        <>
+            <Layout>
+                <div className='h-screen flex justify-center items-center'>
+                    <h2 className='font-semibold text-3xl text-red-600'>Error 404 : Page Not Found</h2>
+                </div>
+            </Layout>
+        </>
     )
 }
 
