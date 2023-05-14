08bde1c902b965522935a57233d390d4871c24ec
Yubraj <imyubraz@gmail.com>
2023-05-14 16:09:39 +0545

    Add Contact form's input binding done with corresponding states..

--- src/pages/add-contact/AddContact.jsx
+++ src/pages/add-contact/AddContact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../layouts/Layout';
 
 
@@ -10,11 +10,19 @@ const Label = ({ inputId, children }) => {
     )
 }
 
-const Input = ({ type, name, id, placeholder }) => {
+const Input = ({ type, name, id, placeholder, changeHandler, value }) => {
     return (
-        <input type={type} name={name} id={id} placeholder={placeholder} className='border-2 outline-1 outline-purple-800/60 px-4 py-2' />
+        <input type={type} name={name} id={id} placeholder={placeholder} onChange={changeHandler} value={value} className='border-2 outline-1 outline-purple-800/60 px-4 py-2' />
     )
 }
+
+/* 
+const FileInput = ({ name, id }) => {
+    return (
+        <input type="file" name={name} id={id} onChange={changeHandler} />
+    )
+}
+*/
 
 const Button = ({ type, id, children }) => {
     return (
@@ -25,6 +33,20 @@ const Button = ({ type, id, children }) => {
 
 const AddContact = () => {
 
+    //states
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitted !")
+
+        console.log(name, email, phone, selectedFile);
+    }
+
+
     return (
         <>
             <Layout>
@@ -34,27 +56,33 @@ const AddContact = () => {
                             <h2 className='text-3xl font-bold'>Add New Contact</h2>
                         </div>
                         <div className='row p-2 mt-6'>
-                            <div className="form-wrapper w-1/2">
-                                <form action="">
+                            <div className="form-wrapper md:w-1/2">
+                                <form action="" onSubmit={handleSubmit}>
                                     <div className="row flex flex-col gap-4">
                                         <div className="row field flex flex-col">
                                             <Label inputId={`nameInput`}>Name</Label>
-                                            <Input type={`text`} name={`name`} id={`nameInput`} />
+                                            <Input type={`text`} name={`name`} id={`nameInput`} value={name} changeHandler={(e) => { setName(e.target.value) }} />
                                             {/* <label htmlFor="nameInput">Name</label> */}
-                                            {/* <input type="text" name="name" id="nameInput" /> */}
+                                            {/* <input type="text" name="name" id="nameInput" value={name} onChange={(e) => { setPhone(e.target.value) }} /> */}
                                         </div>
                                         <div className="row field flex flex-col">
                                             <Label inputId={`emailInput`}>Email</Label>
-                                            <Input type={`email`} name={`email`} id={`emailInput`} />
+                                            <Input type={`email`} name={`email`} id={`emailInput`} value={email} changeHandler={(e) => { setEmail(e.target.value) }} />
                                             {/* <label htmlFor="emailInput">Email</label> */}
-                                            {/* <input type="email" name="email" id="emailInput" /> */}
+                                            {/* <input type="email" name="email" id="emailInput" value={email} onChange={(e) => { setPhone(e.target.value) }} /> */}
                                         </div>
                                         <div className="row field flex flex-col">
                                             <Label inputId={`phoneInput`}>Phone</Label>
-                                            <Input type={`number`} name={`phone`} id={`phoneInput`} />
+                                            <Input type={`number`} name={`phone`} id={`phoneInput`} value={phone} changeHandler={(e) => { setPhone(e.target.value) }} />
                                             {/* <label htmlFor="phoneInput">Phone</label> */}
-                                            {/* <input type="number" name="phone" id="phoneInput" /> */}
+                                            {/* <input type="number" name="phone" id="phoneInput" value={phone} onChange={(e) => { setPhone(e.target.value) }} /> */}
                                         </div>
+                                        <div className="row field flex flex-col">
+                                            <Label inputId={`imageInput`}>Image</Label>
+                                            <input type="file" id='imageInput' onChange={(e) => { setSelectedFile(e.target.files[0]) }} />
+                                            {/* <FileInput id={`imageInput`} name={`image`} changeHandler={(e) => { setSelectedFile(e.target.files[0]) }} /> */}
+                                        </div>
+
                                         <div className="row field flex flex-col">
                                             <Button type={`submit`} id={`addBtn`}>Add</Button>
                                             {/* <button type='submit' id='addBtn'>Add</button> */}
