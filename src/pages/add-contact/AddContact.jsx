17d82e98bce7f8fc63b41a01d4837e99bb88b2ff
Yubraj <imyubraz@gmail.com>
2023-05-14 16:38:42 +0545

    axios package installed and POST request made from Add Contact form, tested and working properly.

--- src/pages/add-contact/AddContact.jsx
+++ src/pages/add-contact/AddContact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Layout from '../../layouts/Layout';
+import axios from 'axios';
 
 
 // Components
@@ -41,61 +42,87 @@ const AddContact = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submitted !")
-
+        console.log("Submitted !");
         console.log(name, email, phone, selectedFile);
+
+        // for sending file to backend we need to create object of FormData
+        const formData = new FormData();
+
+        // formData.append("<name>", "<value>");
+        // formData.append(key, value);
+
+        formData.append("name", name);
+        formData.append("email", email);
+        formData.append("phone", phone);
+        formData.append("image", selectedFile);
+
+        // axios.post(<URL>, <Data>);
+
+        const baseURL = `http://localhost:4444`
+        const endPoint = `contact`
+        // const URL = `localhost:4444/contact`;
+        const URL = `${baseURL}/${endPoint}`;
+
+        try {
+            axios.post(URL, formData)
+            .then(res => console.log(res))
+            .catch (err => console.log(err))
+        } 
+        catch (error) {
+            console.log(error);
+        }
     }
 
-
-    return (
-        <>
-            <Layout>
-                <div className="wrapper">
-                    <div className="container px-2 py-8">
-                        <div className='row'>
-                            <h2 className='text-3xl font-bold'>Add New Contact</h2>
-                        </div>
-                        <div className='row p-2 mt-6'>
-                            <div className="form-wrapper md:w-1/2">
-                                <form action="" onSubmit={handleSubmit}>
-                                    <div className="row flex flex-col gap-4">
-                                        <div className="row field flex flex-col">
-                                            <Label inputId={`nameInput`}>Name</Label>
-                                            <Input type={`text`} name={`name`} id={`nameInput`} value={name} changeHandler={(e) => { setName(e.target.value) }} />
-                                            {/* <label htmlFor="nameInput">Name</label> */}
-                                            {/* <input type="text" name="name" id="nameInput" value={name} onChange={(e) => { setPhone(e.target.value) }} /> */}
-                                        </div>
-                                        <div className="row field flex flex-col">
-                                            <Label inputId={`emailInput`}>Email</Label>
-                                            <Input type={`email`} name={`email`} id={`emailInput`} value={email} changeHandler={(e) => { setEmail(e.target.value) }} />
-                                            {/* <label htmlFor="emailInput">Email</label> */}
-                                            {/* <input type="email" name="email" id="emailInput" value={email} onChange={(e) => { setPhone(e.target.value) }} /> */}
-                                        </div>
-                                        <div className="row field flex flex-col">
-                                            <Label inputId={`phoneInput`}>Phone</Label>
-                                            <Input type={`number`} name={`phone`} id={`phoneInput`} value={phone} changeHandler={(e) => { setPhone(e.target.value) }} />
-                                            {/* <label htmlFor="phoneInput">Phone</label> */}
-                                            {/* <input type="number" name="phone" id="phoneInput" value={phone} onChange={(e) => { setPhone(e.target.value) }} /> */}
-                                        </div>
-                                        <div className="row field flex flex-col">
-                                            <Label inputId={`imageInput`}>Image</Label>
-                                            <input type="file" id='imageInput' onChange={(e) => { setSelectedFile(e.target.files[0]) }} />
-                                            {/* <FileInput id={`imageInput`} name={`image`} changeHandler={(e) => { setSelectedFile(e.target.files[0]) }} /> */}
-                                        </div>
-
-                                        <div className="row field flex flex-col">
-                                            <Button type={`submit`} id={`addBtn`}>Add</Button>
-                                            {/* <button type='submit' id='addBtn'>Add</button> */}
-                                        </div>
+return (
+    <>
+        <Layout>
+            <div className="wrapper">
+                <div className="container px-2 py-8">
+                    <div className='row'>
+                        <h2 className='text-3xl font-bold'>Add New Contact</h2>
+                    </div>
+                    <div className='row p-2 mt-6'>
+                        <div className="form-wrapper md:w-1/2">
+                            <form action="" onSubmit={handleSubmit}>
+                                <div className="row flex flex-col gap-4">
+                                    <div className="row field flex flex-col">
+                                        <Label inputId={`nameInput`}>Name</Label>
+                                        <Input type={`text`} name={`name`} id={`nameInput`} value={name} changeHandler={(e) => { setName(e.target.value) }} />
+                                        {/* <label htmlFor="nameInput">Name</label> */}
+                                        {/* <input type="text" name="name" id="nameInput" value={name} onChange={(e) => { setPhone(e.target.value) }} /> */}
                                     </div>
-                                </form>
-                            </div>
+                                    <div className="row field flex flex-col">
+                                        <Label inputId={`emailInput`}>Email</Label>
+                                        <Input type={`email`} name={`email`} id={`emailInput`} value={email} changeHandler={(e) => { setEmail(e.target.value) }} />
+                                        {/* <label htmlFor="emailInput">Email</label> */}
+                                        {/* <input type="email" name="email" id="emailInput" value={email} onChange={(e) => { setPhone(e.target.value) }} /> */}
+                                    </div>
+                                    <div className="row field flex flex-col">
+                                        <Label inputId={`phoneInput`}>Phone</Label>
+                                        <Input type={`number`} name={`phone`} id={`phoneInput`} value={phone} changeHandler={(e) => { setPhone(e.target.value) }} />
+                                        {/* <label htmlFor="phoneInput">Phone</label> */}
+                                        {/* <input type="number" name="phone" id="phoneInput" value={phone} onChange={(e) => { setPhone(e.target.value) }} /> */}
+                                    </div>
+                                    <div className="row field flex flex-col">
+                                        <Label inputId={`imageInput`}>Image</Label>
+                                        <input type="file" name='image' id='imageInput' onChange={(e) => { setSelectedFile(e.target.files[0]) }} />
+                                        {/* <FileInput id={`imageInput`} name={`image`} changeHandler={(e) => { setSelectedFile(e.target.files[0]) }} /> */}
+                                    </div>
+
+                                    <div className="row field flex flex-col">
+                                        <Button type={`submit`} id={`addBtn`}>Add</Button>
+                                        {/* <button type='submit' id='addBtn'>Add</button> */}
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </Layout>
-        </>
-    )
+            </div>
+        </Layout>
+    </>
+)
 }
+
 
 export default AddContact
