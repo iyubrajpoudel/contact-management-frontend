a5fa07d8e8c348be7e64048d039d14a82e08d69c
Yubraj <imyubraz@gmail.com>
2023-05-16 10:54:10 +0545

    Footer updated...

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Footer = () => {
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
     return (
         <>
-            <div className="wrapper min-h-[10vh] bg-purple-900 text-white">
-                <div className="container"> Footer</div>
+            <div className="wrapper min-h-[10vh] bg-purple-900 text-white flex">
+                <div className="container flex h-full justify-center items-center my-auto">
+                    <div className="row">
+                        <p className='text-lg'>Copyright © {currentYear} Yubraj Poudel. All rights reserved.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
